2f871d3a41ad42ee2e1467526ec46d09d1634dff
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-02-10 12:21:56 +0100

    Update D3, get rid of normalize.css and jQuery and some more cleanup

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(function() {
-  d3.xml('worldmap.svg', 'image/svg+xml', function(xml) {
-    $('#svg').html(xml.documentElement);
+(function() {
+  d3.xml('worldmap.svg').mimeType('image/svg+xml').get(function(error, xml) {
+    document.querySelector('#svg').appendChild(xml.documentElement);
 
     d3.selectAll('path').each(function() {
-      $(this).attr('fill', '#ffce5d');
+      d3.select(this).attr('fill', '#ffce5d');
     });
 
     d3.selectAll('g').on('mouseover', function () {
@@ -16,13 +16,13 @@ $(function() {
       d3.selectAll('#' + this.id).classed('hover', true);
     });
 
-    $(countries).each(function() {
-      $('#' + this).css('fill', '#c0442c');
-      $('#' + this + ' path').css('fill', '#c0442c');
+    countries.map(function(country) {
+      d3.select('#' + country).style('fill', '#c0442c');
+      d3.select('#' + country + ' path').style('fill', '#c0442c');
     });
 
-    $('#number-countries').text(countries.length);
-    $('#globe-percent').text(Math.round(100 * countries.length / 193) + '%');
+    d3.select('#number-countries').text(countries.length);
+    d3.select('#globe-percent').text(Math.round(100 * countries.length / 193) + '%');
 
     function parseId(id) {
       return id.replace(/_/g, '  ').toUpperCase();
@@ -39,27 +39,26 @@ $(function() {
 
       var color = ['#EA2E49', '#77C4D3'];
 
-      var pie = d3.layout.pie()
+      var pie = d3.pie()
           .sort(null);
 
-      var arc = d3.svg.arc()
+      var arc = d3.arc()
           .innerRadius(radius - 100)
           .outerRadius(radius - 50);
 
-      var svg = d3.select("#pie-chart").append("svg")
-          .attr("width", width)
-          .attr("height", height)
-          .append("g")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      var svg = d3.select('#pie-chart').append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-      var path = svg.selectAll("path")
+      var path = svg.selectAll('path')
           .data(pie(dataset.apples))
-          .enter().append("path")
-          .attr("fill", function(d, i) { return color[i]; })
-          .attr("d", arc);
+          .enter().append('path')
+          .attr('fill', function(d, i) { return color[i]; })
+          .attr('d', arc);
     }
 
     donnutChart();
-
   });
-});
+})();
