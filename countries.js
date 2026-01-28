2829f96994279e68937609c98c7d7a3f1f935014
Glauber Ramos <glauberamos@gmail.com>
2026-01-28 10:54:08 +0800

    Fix countries names

--- countries.js
+++ countries.js
@@ -1,42 +1,43 @@
 countries = [
-  "brazil",
-  "ecuador",
-  "chile",
-  "argentina",
-  "uruguay",
-  "paraguay",
-  "peru",
-  "bolivia",
-  "colombia",
-  "usa",
-  "ireland",
-  "britain",
-  "france",
-  "italy",
-  "netherlands",
-  "czech",
-  "belgium",
-  "spain",
-  "germany",
-  "haiti",
-  "south_africa",
-  "uganda",
-  "mozambique",
-  "nigeria",
-  "india",
-  "thailand",
-  "java irian_jaya sulawesi sumatra kalimantan",
-  "turkey",
-  "croatia",
-  "hungary",
-  "portugal",
-  "austria",
-  "monaco",
-  "switzerland",
-  "serbia_and_montenegro",
-  "albania",
-  "bosnia",
-  "greece",
-  "singapore",
-  "vietnam",
+  "BR", // Brazil
+  "EC", // Ecuador
+  "CL", // Chile
+  "AR", // Argentina
+  "UY", // Uruguay
+  "PY", // Paraguay
+  "PE", // Peru
+  "BO", // Bolivia
+  "CO", // Colombia
+  "US", // USA
+  "IE", // Ireland
+  "GB", // United Kingdom
+  "FR", // France
+  "IT", // Italy
+  "NL", // Netherlands
+  "CZ", // Czech Republic
+  "BE", // Belgium
+  "ES", // Spain
+  "DE", // Germany
+  "HT", // Haiti
+  "ZA", // South Africa
+  "UG", // Uganda
+  "MZ", // Mozambique
+  "NG", // Nigeria
+  "IN", // India
+  "TH", // Thailand
+  "ID", // Indonesia
+  "TR", // Turkey
+  "HR", // Croatia
+  "HU", // Hungary
+  "PT", // Portugal
+  "AT", // Austria
+  "MC", // Monaco
+  "CH", // Switzerland
+  "RS", // Serbia
+  "ME", // Montenegro
+  "AL", // Albania
+  "BA", // Bosnia
+  "GR", // Greece
+  "SG", // Singapore
+  "VN", // Vietnam
 ];
