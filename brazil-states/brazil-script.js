7bf54178f2226d651eafd0ba4200359e6c6b428d
Glauber Ramos <glauberamos@gmail.com>
2026-01-28 20:29:52 +0800

    More design changes

--- brazil-states/brazil-script.js
+++ brazil-states/brazil-script.js
@@ -1,3 +1,33 @@
+const stateNames = {
+  BRAC: "Acre",
+  BRAL: "Alagoas",
+  BRAP: "Amapá",
+  BRAM: "Amazonas",
+  BRBA: "Bahia",
+  BRCE: "Ceará",
+  BRDF: "Distrito Federal",
+  BRES: "Espírito Santo",
+  BRGO: "Goiás",
+  BRMA: "Maranhão",
+  BRMT: "Mato Grosso",
+  BRMS: "Mato Grosso do Sul",
+  BRMG: "Minas Gerais",
+  BRPA: "Pará",
+  BRPB: "Paraíba",
+  BRPR: "Paraná",
+  BRPE: "Pernambuco",
+  BRPI: "Piauí",
+  BRRJ: "Rio de Janeiro",
+  BRRN: "Rio Grande do Norte",
+  BRRS: "Rio Grande do Sul",
+  BRRO: "Rondônia",
+  BRRR: "Roraima",
+  BRSC: "Santa Catarina",
+  BRSP: "São Paulo",
+  BRSE: "Sergipe",
+  BRTO: "Tocantins",
+};
+
 (function () {
   d3.xml("br.svg")
     .mimeType("image/svg+xml")
@@ -6,18 +36,55 @@
 
       document.querySelector("#svg").appendChild(xml.documentElement);
 
+      // Fix SVG sizing for proper display
+      const svg = d3.select("#svg svg");
+      svg.attr("width", "100%");
+      svg.attr("height", "100%");
+      svg.attr("viewBox", "0 0 1000 912");
+      svg.attr("preserveAspectRatio", "xMidYMid meet");
+
       d3.selectAll("path").each(function () {
-        d3.select(this).attr("fill", "#ffce5d");
+        d3.select(this).attr("fill", "#e8dcc8");
+        d3.select(this).attr("stroke", "#1a3a3a");
+        d3.select(this).attr("stroke-width", "0.5");
       });
+
+      const tooltip = d3.select("#country-tooltip");
 
       d3.selectAll("path").on("mouseover", function () {
         d3.selectAll(".hover").classed("hover", false);
         d3.select(this).classed("hover", true);
-        displayStateName(this.id);
+        showTooltip(this.id, d3.event);
+      }).on("mousemove", function () {
+        moveTooltip(d3.event);
+      }).on("mouseout", function () {
+        d3.selectAll(".hover").classed("hover", false);
+        hideTooltip();
       });
 
+      function showTooltip(stateId, event) {
+        const stateName = stateNames[stateId] || stateId;
+        tooltip.text(stateName);
+        tooltip.classed("visible", true);
+        if (event) {
+          moveTooltip(event);
+        }
+      }
+
+      function moveTooltip(event) {
+        const tooltipNode = tooltip.node();
+        const tooltipWidth = tooltipNode.offsetWidth;
+        tooltip
+          .style("left", (event.pageX - tooltipWidth / 2) + "px")
+          .style("top", (event.pageY - 60) + "px");
+      }
+
+      function hideTooltip() {
+        tooltip.classed("visible", false);
+      }
+
       brazilStates.forEach(function (state) {
-        d3.select("#BR" + state).style("fill", "#c0442c");
+        d3.select("#BR" + state).style("fill", "#e07a5f");
       });
 
       d3.select("#number-states").text(brazilStates.length);
@@ -26,38 +93,3 @@
       );
     });
 })();
-
-function displayStateName(stateId) {
-  const stateNames = {
-    BRAC: "Acre",
-    BRAL: "Alagoas",
-    BRAP: "Amapá",
-    BRAM: "Amazonas",
-    BRBA: "Bahia",
-    BRCE: "Ceará",
-    BRDF: "Distrito Federal",
-    BRES: "Espírito Santo",
-    BRGO: "Goiás",
-    BRMA: "Maranhão",
-    BRMT: "Mato Grosso",
-    BRMS: "Mato Grosso do Sul",
-    BRMG: "Minas Gerais",
-    BRPA: "Pará",
-    BRPB: "Paraíba",
-    BRPR: "Paraná",
-    BRPE: "Pernambuco",
-    BRPI: "Piauí",
-    BRRJ: "Rio de Janeiro",
-    BRRN: "Rio Grande do Norte",
-    BRRS: "Rio Grande do Sul",
-    BRRO: "Rondônia",
-    BRRR: "Roraima",
-    BRSC: "Santa Catarina",
-    BRSP: "São Paulo",
-    BRSE: "Sergipe",
-    BRTO: "Tocantins",
-  };
-
-  const stateName = stateNames[stateId] || stateId;
-  document.getElementById("country-name").innerHTML = stateName;
-}
